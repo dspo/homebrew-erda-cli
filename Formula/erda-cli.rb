b81d24183b9e5b42c9985fad8b12ee5dc8f8214c
悟空 <rainchan365@163.com>
2022-04-15 11:59:20 +0800

    feat: system

--- Formula/erda-cli.rb
+++ Formula/erda-cli.rb
@@ -18,6 +18,7 @@ class ErdaCli < Formula
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     # system "./configure", *std_configure_args, "--disable-silent-rules"
     # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "erda-cli"
   end
 
   test do
